f94dd7e4ef35c70e09d71bc14ad2f2f00a3ebb7a
Joel Kirchartz <jkirchartz@gmail.com>
2017-04-09 07:27:47 +0200

    fix tests

--- test.js
+++ test.js
@@ -6,21 +6,60 @@
  */
 var gleech = require("./gleech.js");
 
+// one test per function
 gleech.read("test.jpg", function (err, img) {
         if (err) throw err;
-        // one test per function
         img.dither8Bit().write("test_dither8Bit.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.ditherHalftone().write("test_ditherHalftone.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.ditherAtkinsons().write("test_ditherAtkinsons.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.ditherFloydSteinberg().write("test_ditherFloydSteinberg.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.ditherBayer().write("test_ditherBayer.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.ditherBayer3().write("test_ditherBayer3.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.ditherRandom().write("test_ditherRandom.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.ditherBitmask().write("test_ditherBitmask.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.colorShift().write("test_colorShift.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.colorShift2().write("test_colorShift2.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.greenShift().write("test_greenShift.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.redShift().write("test_redShift.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.blueShift().write("test_blueShift.jpg");
+});
+gleech.read("test.jpg", function (err, img) {
+        if (err) throw err;
         img.superShift().write("test_superShift.jpg");
 });
