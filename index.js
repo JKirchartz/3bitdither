5eecc3b8550deab188e6145e581cb8373d931026
Joel Kirchartz <jkirchartz@gmail.com>
2017-11-04 12:49:09 -0400

    try to package to be importable or run from the CLI

--- index.js
+++ index.js
@@ -1,49 +1,7 @@
-#!/usr/bin/env node
-'use strict';
 /*
- * Gleech
+ * index.js
  * Copyright (C) 2017 jkirchartz <me@jkirchartz.com>
  *
- * Distributed under terms of the GPL3.0 license.
+ * Distributed under terms of the NPL (Necessary Public License) license.
  */
-var gleech = require('./dist/gleech.js');
-var cmdr = require('commander');
-
-cmdr
-.command('glitch <input> <output> [operation] [parameters...]')
-.description('Glitches an image with optional type and parameters')
-.action(function(input, output, operation, parameters) {
-	gleech.read(input, function(err, image) {
-		if (operation && parameters) {
-			image[operation].apply(this, parameters);
-		} else if (operation && !parameters) {
-			image[operation]();
-		} else {
-			console.log("please provide a function name");
-		}
-		image.write(output);
-	});
-});
-
-cmdr.command('list')
-.description('Lists available functions')
-.action(function() {
-	var output = "";
-	output += ('\nGlitches:\n\n');
-	for (var prop in gleech.prototype) {
-		if (typeof gleech.prototype[prop] === 'function' &&
-				gleech.prototype[prop].name){
-			output += gleech.prototype[prop].name + ' ';
-		}
-	}
-	output += ('\n\nJimp functions:\n\n');
-	for (var prop2 in gleech.prototype) {
-		if (typeof gleech.prototype[prop2] === 'function' &&
-				! gleech.prototype[prop2].name){
-			output += String(prop2) + ' ';
-		}
-	}
-	console.log(output);
-});
-
-cmdr.version('0.1.0').parse(process.argv);
+module.exports = require('./dist/gleech');
